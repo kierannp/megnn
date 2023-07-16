212bfbca63e51d98c70d60d23d3554405b89ec6d
Kieran Nehil-Puleo <kierannp@head.cl.vanderbilt.edu>
2023-07-15 19:57:00 -0500

    Added unfinsihed convolution

--- megnn/megnn.py
+++ megnn/megnn.py
@@ -102,6 +102,81 @@ class GCL(GCL_basic):
             #out = self.gru(out, h)
         return out
 
+class I_GCL(GCL_basic):
+    """Graph Neural Net with global state and fixed number of nodes per graph.
+    Args:
+          hidden_dim: Number of hidden units.
+          num_nodes: Maximum number of nodes (for self-attentive pooling).
+          global_agg: Global aggregation function ('attn' or 'sum').
+          temp: Softmax temperature.
+    """
+
+    def __init__(self, input_nf, output_nf, hidden_nf, int_node_nf, edges_in_nf=0, act_fn=nn.ReLU(), bias=True, attention=False, t_eq=False, recurrent=True):
+        super(I_GCL, self).__init__()
+        self.attention = attention
+        self.t_eq=t_eq
+        self.recurrent = recurrent
+        input_edge_nf = input_nf * 2
+        self.edge_mlp = nn.Sequential(
+            nn.Linear(input_edge_nf + edges_in_nf, hidden_nf, bias=bias),
+            act_fn,
+            nn.Linear(hidden_nf, hidden_nf, bias=bias),
+            act_fn)
+        
+        if self.attention:
+            self.att_mlp = nn.Sequential(
+                nn.Linear(input_nf, hidden_nf, bias=bias),
+                act_fn,
+                nn.Linear(hidden_nf, 1, bias=bias),
+                nn.Sigmoid())
+
+        self.interaction_mlp = nn.Sequential(
+            nn.Linear(int_node_nf, hidden_nf, bias=bias),
+            act_fn,
+            nn.Linear(hidden_nf, hidden_nf, bias=bias),
+            act_fn)
+
+        self.node_mlp = nn.Sequential(
+            nn.Linear(hidden_nf + input_nf, hidden_nf, bias=bias),
+            act_fn,
+            nn.Linear(hidden_nf, output_nf, bias=bias))
+        
+
+    def edge_model(self, source, target, edge_attr):
+        edge_in = torch.cat([source, target], dim=1)
+        if edge_attr is not None:
+            edge_in = torch.cat([edge_in, edge_attr], dim=1)
+        out = self.edge_mlp(edge_in)
+        if self.attention:
+            att = self.att_mlp(torch.abs(source - target))
+            out = out * att
+        return out
+
+    def interaction_model(self, source, target):
+        edge_in = torch.cat([source, target], dim=1)
+        out = self.interaction_mlp(edge_in)
+        if self.attention:
+            att = self.att_mlp(torch.abs(source - target))
+            out = out * att
+        return out
+    
+    def node_model(self, h, edge_index, int_index, edge_attr, int_attr):
+        row, col = edge_index
+        edge_agg = unsorted_segment_sum(edge_attr, row, num_segments=h.size(0))
+        int_agg = 
+        out = torch.cat([h, edge_agg, int_agg], dim=1)
+        out = self.node_mlp(out)
+        if self.recurrent:
+            out = out + h
+        return out
+    
+    def forward(self, x, edge_index, int_x, edge_attr = None):
+        row, col = edge_index
+        edge_feat = self.edge_model(x[row], x[col], edge_attr)
+        int_feat = self.interaction_model(x, int_x)
+        x = self.node_model(x, edge_index, edge_feat, int_feat)
+        return x, edge_feat
+    
 class GCL_rf(GCL_basic):
     """Graph Neural Net with global state and fixed number of nodes per graph.
     Args:
@@ -229,6 +304,120 @@ class E_GCL(nn.Module):
         coord += agg*self.coords_weight
         return coord
 
+
+    def coord2radial(self, edge_index, coord):
+        row, col = edge_index
+        coord_diff = coord[row] - coord[col]
+        radial = torch.sum((coord_diff)**2, 1).unsqueeze(1)
+
+        if self.norm_diff:
+            norm = torch.sqrt(radial) + 1
+            coord_diff = coord_diff/(norm)
+
+        return radial, coord_diff
+
+    def forward(self, h, edge_index, coord, edge_attr=None, node_attr=None):
+        row, col = edge_index
+        radial, coord_diff = self.coord2radial(edge_index, coord)
+
+        edge_feat = self.edge_model(h[row], h[col], radial, edge_attr)
+        coord = self.coord_model(coord, edge_index, coord_diff, edge_feat)
+        h, agg = self.node_model(h, edge_index, edge_feat, node_attr)
+        # coord = self.node_coord_model(h, coord)
+        # x = self.node_model(x, edge_index, x[col], u, batch)  # GCN
+        return h, coord, edge_attr
+
+# TODO implement I_E_GCL
+class I_E_GCL(nn.Module):
+    """Graph Neural Net with global state and fixed number of nodes per graph.
+    Args:
+          hidden_dim: Number of hidden units.
+          num_nodes: Maximum number of nodes (for self-attentive pooling).
+          global_agg: Global aggregation function ('attn' or 'sum').
+          temp: Softmax temperature.
+    """
+
+    def __init__(self, input_nf, output_nf, hidden_nf, edges_in_d=0, nodes_att_dim=0, act_fn=nn.ReLU(), recurrent=True, coords_weight=1.0, attention=False, clamp=False, norm_diff=False, tanh=False):
+        super(I_E_GCL, self).__init__()
+        input_edge = input_nf * 2
+        self.coords_weight = coords_weight
+        self.recurrent = recurrent
+        self.attention = attention
+        self.norm_diff = norm_diff
+        self.tanh = tanh
+        edge_coords_nf = 1
+
+
+        self.edge_mlp = nn.Sequential(
+            nn.Linear(input_edge + edge_coords_nf + edges_in_d, hidden_nf),
+            act_fn,
+            nn.Linear(hidden_nf, hidden_nf),
+            act_fn,
+            nn.Linear(hidden_nf, hidden_nf),
+            act_fn)
+
+        self.node_mlp = nn.Sequential(
+            nn.Linear(hidden_nf + input_nf + nodes_att_dim, hidden_nf),
+            act_fn,
+            nn.Linear(hidden_nf, hidden_nf),
+            act_fn,
+            nn.Linear(hidden_nf, output_nf))
+
+        layer = nn.Linear(hidden_nf, 1, bias=False)
+        torch.nn.init.xavier_uniform_(layer.weight, gain=0.001)
+
+        self.clamp = clamp
+        coord_mlp = []
+        coord_mlp.append(nn.Linear(hidden_nf, hidden_nf))
+        coord_mlp.append(act_fn)
+        coord_mlp.append(nn.Linear(hidden_nf, hidden_nf))
+        coord_mlp.append(act_fn)
+        coord_mlp.append(layer)
+        if self.tanh:
+            coord_mlp.append(nn.Tanh())
+            self.coords_range = nn.Parameter(torch.ones(1))*3
+        self.coord_mlp = nn.Sequential(*coord_mlp)
+
+
+        if self.attention:
+            self.att_mlp = nn.Sequential(
+                nn.Linear(hidden_nf, 1),
+                nn.Sigmoid())
+
+        #if recurrent:
+        #    self.gru = nn.GRUCell(hidden_nf, hidden_nf)
+
+
+    def edge_model(self, source, target, radial, edge_attr):
+        if edge_attr is None:  # Unused.
+            out = torch.cat([source, target, radial], dim=1)
+        else:
+            out = torch.cat([source, target, radial, edge_attr], dim=1)
+        out = self.edge_mlp(out)
+        if self.attention:
+            att_val = self.att_mlp(out)
+            out = out * att_val
+        return out
+
+    def node_model(self, x, edge_index, edge_attr, node_attr):
+        row, col = edge_index
+        agg = unsorted_segment_sum(edge_attr, row, num_segments=x.size(0))
+        if node_attr is not None:
+            agg = torch.cat([x, agg, node_attr], dim=1)
+        else:
+            agg = torch.cat([x, agg], dim=1)
+        out = self.node_mlp(agg)
+        if self.recurrent:
+            out = x + out
+        return out, agg
+
+    def coord_model(self, coord, edge_index, coord_diff, edge_feat):
+        row, col = edge_index
+        trans = coord_diff * self.coord_mlp(edge_feat)
+        trans = torch.clamp(trans, min=-100, max=100) #This is never activated but just in case it case it explosed it may save the train
+        agg = unsorted_segment_mean(trans, row, num_segments=coord.size(0))
+        coord += agg*self.coords_weight
+        return coord
 
     def coord2radial(self, edge_index, coord):
         row, col = edge_index
@@ -426,7 +615,7 @@ class MEGNN(nn.Module):
         return pred.squeeze(1)
 
 class MGNN(torch.nn.Module):
-    def __init__(self, n_graphs, in_node_nf, in_edge_nf, device, hidden_nf=128, n_layers=8, coords_weight=1., act_fn=nn.ReLU()):
+    def __init__(self, n_graphs, in_node_nf, in_edge_nf, device, hidden_nf=128, n_layers=8, act_fn=nn.ReLU()):
         super(MGNN, self).__init__()
         self.hidden_nf = hidden_nf
         self.device = device
@@ -470,4 +659,34 @@ class MGNN(torch.nn.Module):
         combined = torch.cat(hf, dim=1)
         pred = self.grand_dec(combined)
         return pred.squeeze(1)
-        
+
+class IGNN(torch.nn.Module):
+    def __init__(self, in_node_nf, in_edge_nf, device, hidden_nf=128, n_layers=8,  node_attr=1, act_fn=nn.ReLU(), attention=False):
+        super(IGNN, self).__init__()
+        self.hidden_nf = hidden_nf
+        self.device = device
+        self.n_layers = n_layers
+
+        ### Encoder
+        self.embedding = nn.Linear(in_node_nf, hidden_nf)
+        self.node_attr = node_attr
+        if node_attr:
+            n_node_attr = in_node_nf
+        else:
+            n_node_attr = 0
+        for i in range(0, n_layers):
+            self.add_module("i_gcl_%d" % i, I_GCL(self.hidden_nf, self.hidden_nf, self.hidden_nf, int_edge_nf=in_node_nf,edges_in_d=in_edge_nf, nodes_attr_dim=n_node_attr, act_fn=act_fn, recurrent=True, attention=attention))
+
+        self.node_dec = nn.Sequential(nn.Linear(self.hidden_nf, self.hidden_nf),
+                                      act_fn,
+                                      nn.Linear(self.hidden_nf, self.hidden_nf))
+        self.to(self.device)
+
+    def forward(self, h, edges, edge_attr, int_h):
+        h = self.embedding(h)
+        for i in range(0, self.n_layers):
+            h = self._modules["i_gcl_{}_{}".format(i)](h, edges, edge_attr, int_h)
+        h = self._modules["node_dec"](h)
+        h = h.unsqueeze(0)
+        pred = torch.sum(h, dim=1)
+        return pred.squeeze(1)
